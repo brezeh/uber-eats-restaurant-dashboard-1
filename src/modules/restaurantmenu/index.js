f3b733f50fd5d9c334b14c5f3fb5f0b37bf5a851
brezeh <howardb@chc.edu>
2025-02-05 21:57:39 -0500

    Update index.js

--- src/modules/restaurantmenu/index.js
+++ src/modules/restaurantmenu/index.js
@@ -1,1 +1,38 @@
+import { Card, Table, Button } from "antd";
+import dishes from '.../.../assets/data/dashboard/dishes.json';
+import { Link } from 'react-router-dom'
 
+const restaurantmenu = () => {
+    const tablecoulmns = [
+        {
+            title: "menu item",
+            dataindex: "name",
+            key: "name",
+        },
+        {
+            title: "price",
+            dataindex: 'price',
+            key: 'price',
+            render: (price) => `${price}`
+        },
+        {
+            title: "action",
+            key: 'action',
+            render: () => <Button danger>remove</Button>
+        },
+    ];
+
+    const rendernewitembutton = () => (
+        <Link to={'create'}>
+            <Button type="primary">new item</Button>
+        </Link>
+    )
+
+    return (
+    <Card title={"menu"} style={{ margin: 20}} extra={rendernewitembutton}>
+        <Table dataSource={dishes} columns={tablecoulmns} rowKey="id"/> 
+    </Card>
+    );
+};
+
+export default restaurantmenu;
