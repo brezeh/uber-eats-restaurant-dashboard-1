96df8485a07931dd2393a5e978631cf2bdb323b0
brezeh <howardb@chc.edu>
2025-02-05 21:08:16 -0500

    Update index.js

--- src/components/sidemenu/index.js
+++ src/components/sidemenu/index.js
@@ -1,1 +1,31 @@
+import { Menu } from "antd";
+import { useNavigate } from "react-router-dom";
 
+const sidemenu = () => {
+    const navigate = useNavigate();
+
+    const menuitems =[
+        {
+            key: "/",
+            label: 'orders'
+        },
+        {
+            key: "menu",
+            label: 'menu'
+        },
+        {
+            key: "order-history",
+            label: 'order history'
+        },
+        {
+            key: "settings",
+            label: 'settings'
+        }
+    ];
+
+    return (
+        <Menu items={menuitems} onClick={(menuitems) => navigate(menuitem.key)}/>
+    ) 
+};
+
+export default sidemenu;
