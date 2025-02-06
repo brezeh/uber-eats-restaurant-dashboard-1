6ed6c5f2fca79e7f3d90854247b99c0132d327c6
brezeh <howardb@chc.edu>
2025-02-05 20:24:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import detailedorder from "./modules/detailedorder";
+import Orders from "./modules/detailedorder/orders";
+import { Routes, Route } from "react-router-dom";
+import { Layout, Image } from "antd";
+
+const {Sider, Content, Footer} = Layout;
 
 function App() {
   return (
-    <detailedorder />
+    <Layout>
+      <Sider style={{height: "100vh", backgroundColor: 'white'}}>
+        <Image 
+          src="https://logos-world.net/wp-content/uploads/2020/11/Uber-Eats-Symbol.jpg" 
+          preview={false} 
+        />
+      </Sider>
+      <Layout>
+        <Content>
+          <Routes>
+            <Route path="/" element={<Orders />}/>
+            <Route path="order/:id" element={<detailedorder />}/>
+          </Routes>
+        </Content>
+        <Footer style={{textAlign: 'center'}}>
+          Uber Eats Restaurant Dashboard 2025
+        </Footer>
+      </Layout>
+    </Layout>
   );
 }
-
-
 
 export default App;
