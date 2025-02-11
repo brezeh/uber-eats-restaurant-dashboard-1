620248d31ba848577f294096faf4e04eb7e218e0
brezeh <howardb@chc.edu>
2025-02-10 19:03:37 -0500

    Update index.js

--- src/modules/orderhistory/index.js
+++ src/modules/orderhistory/index.js
@@ -1,1 +1,44 @@
+import ordersHistory from '../../assets/data/orders-history.json';
+import { Card, Table, Tag } from 'antd';
 
+const OrderHistory = () => {
+    
+    const tablecoulmns = [
+        {
+            title: 'order ID',
+            dataindex: 'orderID',
+            key: 'orderID',
+        },
+        {
+            title: 'delivery address',
+            dataindex: 'deliveryAddress',
+            key: 'deliveryAddress'
+        },
+        {
+            title: 'Price',
+            dataindex: 'price',
+            key: 'price',
+            render: (price) => `$${price} $`,
+        },
+        {
+            title: 'Status',
+            dataindex: 'status',
+            key: 'status',
+            render: (status) => (
+            <Tag color={status === 'delivered' ? 'Green' : 'red'}>{status}</Tag>
+            ),
+        },
+    ];
+    
+    return (
+        <Card title={'History Orders'} style={{margin: 20 }}> 
+            <Table
+                datasource={ordersHistory}
+                columns={tablecoulmns} 
+                rowkey="orderID"
+            />
+        </Card> 
+    )
+};
+
+export default OrderHistory;
