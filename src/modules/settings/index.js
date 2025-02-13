27386a38fe48d5873fec21a0be6f4009fbf07ac8
brezeh <howardb@chc.edu>
2025-02-12 20:18:30 -0500

    Update index.js

--- src/modules/settings/index.js
+++ src/modules/settings/index.js
@@ -1,1 +1,20 @@
+import { Form, Input, Card, Button } from "antd";
 
+const Settings = () => {
+    return (
+        <Card title="restaurant details" style={{ margin: 20 }}>
+            <Form layout="verticle" wrapperCol={{ span: 8 }}>
+                <Form.Item label="restaurant name" required>
+                    <Input placeholder="enter restaurant name here"/>
+                </Form.Item>
+                <Form.Item label="restaurant address" required>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary">submit</Button>
+                </Form.Item>
+            </Form>
+        </Card>
+    )
+};
+
+export default Settings;
